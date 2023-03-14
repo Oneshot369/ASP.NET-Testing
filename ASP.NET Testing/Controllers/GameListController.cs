9d7f79da5e4b654e889bd398656d70748e91c4fa
Oneshot369 <joshcpeck4@gmail.com>
2023-03-13 18:44:09 -0700

    added Game view and controller

---
+++ ASP.NET Testing/Controllers/GameListController.cs
@@ -0,0 +1,18 @@
+﻿using ASP.NET_Testing.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace ASP.NET_Testing.Controllers
+{
+    public class GameListController : Controller
+    {
+        public IActionResult Index()
+        {
+            ArrayList games = new ArrayList();
+            games.Add(new Game("001", "Leage of Ledgens", "MOBA", 0.0f, 'E'));
+            games.Add(new Game("002", "Destiny", "FPS, RPG, MMO", 0.0f, 'T'));
+            games.Add(new Game("003", "ARK", "Survial", 60.0f, 'M'));
+            return View(games);
+        }
+    }
+}
--- ASP.NET Testing/Models/Game.cs
+++ ASP.NET Testing/Models/Game.cs
@@ -7,5 +7,18 @@
         public string Genre { get; set; }
         public float Price { get; set; }
         public char Rating { get; set; }
+
+        //Default Contructor
+        public Game(){   }
+
+        // Paramiterized contructor
+        public Game(string id, string name, string genre, float price, char rating)
+        {
+            Id = id;
+            Name = name;
+            Genre = genre;
+            Price = price;
+            Rating = rating;
+        }
     }
 }
