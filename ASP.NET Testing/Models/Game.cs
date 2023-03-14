18d0a776fb557094f7827efa5911528bb017ece1
Oneshot369 <joshcpeck4@gmail.com>
2023-03-13 18:29:12 -0700

    Added Game model class

---
+++ ASP.NET Testing/Models/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NET_Testing.Models
+{
+    public class Game
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public float Price { get; set; }
+        public char Rating { get; set; }
+    }
+}
